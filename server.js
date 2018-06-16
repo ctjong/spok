e1de35fac7f21497fb1f08feb61b284533a32225
ctjong <christophertjong@hotmail.com>
2018-06-16 10:54:21 -0700

    build on server start

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const app = require('express')();
 const path = require('path');
+const { exec } = require('child_process');
+const fs = require('fs');
+
+if(!fs.existsSync(path.join(__dirname, "/client/build")))
+{
+    const buildCmd = `node ${path.join(__dirname, "/client/node_modules/react-scripts/scripts/build.js")}`;
+    console.log(`Executing ${buildCmd}`);
+    process.chdir("./client");
+    exec(buildCmd, (e, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+    });
+}
 
 app.listen(3000, () => 
 {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
 
 app.get("*", (req, res) => 
