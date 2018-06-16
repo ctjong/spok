7c2d8366cfd183a4f4fe87bb17a7e869e6dcd411
ctjong <christophertjong@hotmail.com>
2018-06-16 10:55:41 -0700

    exclude build folder

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const app = require('express')();
 const path = require('path');
 const { exec } = require('child_process');
-const fs = require('fs');
 
-if(!fs.existsSync(path.join(__dirname, "/client/build")))
-{
-    const buildCmd = `node ${path.join(__dirname, "/client/node_modules/react-scripts/scripts/build.js")}`;
-    console.log(`Executing ${buildCmd}`);
-    process.chdir("./client");
-    exec(buildCmd, (e, stdout, stderr) => {
-      console.log(stdout);
-      console.log(stderr);
-    });
-}
+const buildCmd = `node ${path.join(__dirname, "/client/node_modules/react-scripts/scripts/build.js")}`;
+console.log(`Executing ${buildCmd}`);
+process.chdir("./client");
+exec(buildCmd, (e, stdout, stderr) => {
+  console.log(stdout);
+  console.log(stderr);
+});
 
 app.listen(3000, () => 
 {
