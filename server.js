f536de66174c7311b446474f58369d7c6b7820a8
ctjong <christophertjong@hotmail.com>
2018-06-16 12:04:55 -0700

    fixed static file refs

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const app = require('express')();
+const express = require('express');
 const path = require('path');
+
+const app = express();
+app.use('/static', express.static(path.join(__dirname, 'build/static')));
+app.use('/asset-manifest.json', express.static(path.join(__dirname, 'build/asset-manifest.json')));
+app.use('/favicon.ico', express.static(path.join(__dirname, 'build/favicon.ico')));
+app.use('/manifest.json', express.static(path.join(__dirname, 'build/manifest.json')));
+app.use('/service-worker.js', express.static(path.join(__dirname, 'build/service-worker.js')));
 
 app.listen(3000, () => 
 {
@@ -8,5 +15,5 @@ app.listen(3000, () =>
 
 app.get("*", (req, res) => 
 {
-    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+    res.sendFile(path.join(__dirname, "build/index.html"));
 });
