cae36d0d6c1d300253e1cf4a157104b9437c94d6
ctjong <christophertjong@hotmail.com>
2018-06-27 00:04:17 -0700

    removed paper id from ui

--- src/components/roomControls/wait-pane.js
+++ src/components/roomControls/wait-pane.js
@@ -9,8 +9,6 @@ class WaitPane extends Component
     {
         return (
             <div className="pane wait-pane">
-                <h1><span className="user-name"></span>@<span className="room-code"></span></h1>
-                <h2 className="paper-id-heading">Paper #<span className="paper-id"></span></h2>
                 <div>Waiting for:</div>
                 <div className="waitList"></div>
             </div>
--- src/components/roomControls/write-pane.js
+++ src/components/roomControls/write-pane.js
@@ -15,8 +15,6 @@ class WriteView extends Component
     {
         return (
             <div className="pane write-pane">
-                <h1><span className="user-name"></span>@<span className="room-code"></span></h1>
-                <h2 className="paper-id-heading">Paper #<span className="paper-id"></span></h2>
                 <div className="form-inline phrase1">
                     <label></label>
                     <input type="text" className="input form-control"/>
